76a4c80d9b60a60275aec343cf647e0ce81fc02e
jjhiggz <jonathan.higger@gmail.com>
2020-08-09 13:12:42 -0600

    got box to flash yellow

--- Academind Pro Assignments/styling-challenge-1/src/App.vue
+++ Academind Pro Assignments/styling-challenge-1/src/App.vue
@@ -4,15 +4,43 @@
       title="Styling Assignment 1"
       description="1: Alternate between highlight and shrink using a setINterval function, attacth a CSS Class to a div. "
     />
+    <button 
+      @click="startEffect"
+    >
+      START EFFECT
+    </button>
+    <div
+      id="effect"
+      :class="{highlight: highlight}"  
+    >
+
+    </div>
   </div>
 </template>
 
 <script>
-import StyledHeader  from './components/StyledHeader.vue';
+import StyledHeader from "./components/StyledHeader.vue";
 export default {
   name: "App",
+  data() {
+    return {
+      highlight: false,
+    }
+  },
   components: {
     StyledHeader
+  },
+  methods: {
+    startEffect(){
+      let vm = this;
+      let interval = setInterval( () => {
+        vm.highlight = !vm.highlight
+      } ,100)
+      setTimeout(() => {
+        clearInterval(interval)
+      },3000)
+    },
+
   }
 };
 </script>
@@ -22,5 +50,19 @@ body {
   padding: 0px;
   margin: 0px;
   border: 0px;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+#effect {
+  width: 100px;
+  height: 100px;
+  border: 1px solid black
+}
+.highlight {
+  background-color: yellow;
 }
 </style>
