f61bae304909f528a95cee6b76039106a3e283ab
jjhiggz <jonathan.higger@gmail.com>
2020-08-09 17:01:46 -0600

    added contionals and lists assignment

---
+++ Academind Pro Assignments/ConditionalsAndLists/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <StyledHeader
+      title="Title Name Goes Here"
+      description="This is the description or problem statement of the application"
+    />
+  </div>
+</template>
+
+<script>
+import StyledHeader  from './components/StyledHeader.vue';
+export default {
+  name: "App",
+  components: {
+    StyledHeader
+  }
+};
+</script>
+
+<style>
+body {
+  padding: 0px;
+  margin: 0px;
+  border: 0px;
+}
+</style>
